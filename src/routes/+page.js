248d70e08b5a19157c62975611ece8c53b53851d
eliiiphant <125492812+eliiiphant@users.noreply.github.com>
2025-04-29 13:07:24 +0800

    add: live reading updates

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,9 +1,7 @@
 export async function load({ fetch }) {
 	const res = await fetch('/api/arduino-data', { method: 'GET' } );
-	console.log(res);
 	const result = await res.json();
 
-	console.log('HI ' + String(res.ok) + ' ' + JSON.stringify(result));
 	return {
 		reading: result.data ?? null
 	};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,44 @@
-<script>
+<script lang="ts">
 	export let data;
 	const { reading } = data;
+
+	import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
+	import { onMount } from 'svelte';
+	import { writable } from 'svelte/store';
+	import { createClient } from '@supabase/supabase-js';
+  
+	const supabase = createClient(
+	  PUBLIC_SUPABASE_URL,
+	  PUBLIC_SUPABASE_ANON_KEY
+	);
+  
+	const ph = writable<Number | null>(null);
+	const turbidity = writable<Number | null>(null);
+	const lastUpdated = writable<string | null>(null);
+  
+	onMount(() => {
+	  const channel = supabase
+		.channel('realtime:sensor_readings')
+		.on(
+		  'postgres_changes',
+		  {
+			event: 'INSERT',
+			schema: 'public',
+			table: 'sensor_readings'
+		  },
+		  (payload) => {
+			const row = payload.new;
+			ph.set(row.ph);
+			turbidity.set(row.turbidity);
+			lastUpdated.set(String(row.created_at));
+		  }
+		)
+		.subscribe();
+  
+	  return () => {
+		supabase.removeChannel(channel);
+	  };
+	});
 </script>
 
 <title>SOWQMS</title>
@@ -19,18 +57,18 @@
 <div class="bg-gray-100 border-2 border-gray-200 w-full flex flex-col items-center justify-center rounded-md p-5 space-y-6.5">
 	<div class="flex flex-col items-center space-y-1.5">
 		<p>pH Level</p>
-		<p class="text-[40px] mt-[-13px] font-bold">{(reading.ph).toFixed(1)}</p>
+		<p class="text-[40px] mt-[-13px] font-bold">{$ph ?? reading.ph ?? "Waiting..."}</p>
 		<div class="w-50 h-2.5 bg-white rounded-sm border-1 border-gray-200"></div>
 		<p class="text-sm">neutral</p>
 	</div>
 
 	<div class="flex flex-col items-center space-y-1.5">
 		<p>Turbidity</p>
-		<p class="text-[40px] mt-[-13px] font-bold">{(reading.turbidity).toFixed(1)}</p>
+		<p class="text-[40px] mt-[-13px] font-bold">{$turbidity ?? reading.turbidity ??'Waiting...'}</p>
 		<div class="w-50 h-2.5 bg-white rounded-sm border-1 border-gray-200"></div>
 		<p class="text-sm">low</p>
 	</div>
-	<p>Last updated: {reading.created_at}</p>
+	<p>Last updated: {$lastUpdated ?? reading.created_at ?? 'Waiting...'}</p>
 </div>
 
 <p class="text-[25px] font-semibold">Historical data</p>
